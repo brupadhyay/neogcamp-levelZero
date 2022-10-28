781371f22df0f079a0affd16371660ccf8408947
Bhavesh Upadhyay <bh8692080@gmail.com>
2022-10-29 02:07:10 +0530

    mark8 added

---
+++ mark8-EmojiExpress/src/App.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import "./styles.css";
+
+var emojiDB = {
+  "😀": "grinning face",
+  "😎": "smiling face with sunglasses",
+  "😅": "grinning face with sweat",
+  "🤡": "clown face",
+  "😇": "smiling face with halo",
+  "👋": "waving hand",
+  "🙏": "folded hands",
+  "🚀": "rocket",
+  "😜": "winking face with tongue",
+  "🛐": "place of worship",
+  "🔨": "hammer"
+};
+
+const emojis = Object.keys(emojiDB);
+
+export default function App() {
+  const [meaning, setmeaning] = useState("emoji meaning will come here");
+
+  function changeEventHandler(event) {
+    var userInput = emojiDB[event.target.value];
+    if (userInput !== undefined) {
+      setmeaning(userInput);
+    } else {
+      setmeaning("Emoji not found!");
+    }
+  }
+
+  function clickEventHandler(emoji) {
+    var meaning = emojiDB[emoji];
+    setmeaning(meaning);
+  }
+  return (
+    <div className="App">
+      <h1>Emoji Express</h1>
+      <input
+        onChange={changeEventHandler}
+        placeholder={"enter your emoji here"}
+      ></input>
+      <h2>{meaning}</h2>
+      <h3> emojis we know </h3>
+      <ul>
+        {emojis.map((emoji) => {
+          return (
+            <span key={emoji} onClick={() => clickEventHandler(emoji)}>
+              {emoji}
+            </span>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
